366302151fd10a28a6d08911ad7f2be89cf97941
Vanessa Lobo <vanessa.lobo0211@gmail.com>
2020-10-28 19:13:01 +0530

    controlled form

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,12 +5,15 @@ import {
   CardText,
   CardBody,
   CardTitle,
+  BreadcrumbItem,
+  Breadcrumb
 } from "reactstrap";
+import {Link} from 'react-router-dom';
 //UI COMPONENTc
 
 function RenderDish({ dish }) {
   return (
-    <div className="col-12 col-md-5 m-1">
+    <div className="col-12">
       <Card>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardBody>
@@ -39,7 +42,7 @@ function RenderComments({ comments }) {
     );
   });
   return (
-    <div className="col-12 col-md-5 m-1">
+    <div className="col-12">
       <h4>Comments</h4>
       {rencomments}
     </div>
@@ -47,15 +50,30 @@ function RenderComments({ comments }) {
 }
 
 const DishDetail = (props) => {
-  const dish = props.dish;
-  if (props.dish != null) {
-    return (
-      <div className="row">
-        <RenderDish dish={dish} />
-        <RenderComments comments={props.dish.comments} />
-      </div>
-    );
-  } else return null;
+  
+  return (
+    <div className="container">
+    <div className="row">
+        <Breadcrumb>
+
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+        </div>                
+    </div>
+    <div className="row">
+        <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+        </div>
+        <div className="col-12 col-md-5 m-1">
+            <RenderComments comments={props.comments} />
+        </div>
+    </div>
+    </div>
+);
 };
 
 export default DishDetail;
